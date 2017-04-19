4b70b18872ea53b9ff9d727e6bdbc39545b8f8bf
kanyijoshua <kanyiwest.jo@gmail.com>
2017-04-19 09:11:58 +0300

    almost

--- main.js
+++ main.js
@@ -1,28 +1,73 @@
-$(document).ready(function(){
-	$('#seachForm').on('submit', (e) =>{
+$(document).ready(()=>{
+	$('#searchForm').on('submit', (e)=>{
 		let searchText=$('#searchText').val();
-		getMovies(searchtext);
+		getMovies(searchText);
 		e.preventDefault();
 	});
 });
 function getMovies(searchText) {
-	axios.get('http://www.omdbapi.com/?='+searchText)
-		.then((response) => {
+	axios.get('http://www.omdbapi.com?s='+searchText)
+		.then((response)=>{
 		console.log(response);
 		let movies=response.data.Search;
 		let output ='';
-		$.each(movies,(index,movie)=>{
-		output+= `
-			<div class="col-md-3">
-			<div class="well text-center">
-			<img src="${movie.Poster}">
-			<h4>${movie.Title}</h4>
-			</div>
-			</div>
-			`;
+		$.each(movies, (index, movie)=>{
+		    output+=` 
+<div class="col-md-3">
+<div class="well text-center">
+<img src="${movie.Poster}">
+<h5>${movie.Title}</h5>
+<a onclick="movieSelected('$movie.imdbID')" class="btn btn-primary" href="#">movie details</a>
+</div>
+</div>
+`;
 		});
+		$('#movies').html(output);
 })
-	.catch((err) => {
+	.catch(function(err){
+		console.log(err);
+	});
+}
+function movieSelected(id){
+	sessionStorage.setItem('movieId',id);
+	window.location='info.html';
+	return false;
+}
+function getMovie(){
+	let movieId=sessionStorage.getItem('movieId');
+	axios.get('http://www.omdbapi.com?i='+searchText)
+		.then(function(response){
+		console.log(response);
+		let movie=response.data;
+		let output=`
+<div class="row">
+<div class="col-md-4">
+<img src="${movie.Poster}" class="thumbnail">
+</div>
+<div class="col-md-8">
+<h2>${movie.Title}</h2>
+<ul class="list-group">
+<li class="list-group-item"><strong>Genre:</strong>${movie.Genre}</li>
+<li class="list-group-item"><strong>Released:</strong>${movie.Released}</li>
+<li class="list-group-item"><strong>Rated:</strong>${movie.Rated}</li>
+<li class="list-group-item"><strong>IMDB RATING:</strong>${movie.imdbRating}</li>
+<li class="list-group-item"><strong>Director:</strong>${movie.Director}</li>
+<li class="list-group-item"><strong>Writer:</strong>${movie.Writer}</li>
+<li class="list-group-item"><strong>Actors:</strong>${movie.Actors}</li></ul>
+</div>
+</div>
+<div class="row">
+<div class="well">
+${movie.Plot}
+<hr>
+<a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">view IMDB</a>
+<a href="index.html" class= "btn btn-default"> go back to home page</a>
+</div>
+</div>
+`;
+		$('#movie').html(output);
+})
+	.catch(function(err){
 		console.log(err);
 	});
 }
