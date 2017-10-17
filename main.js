184327206248c4e2c5b33ce4fefbca1e922bb41d
kanyijoshua <kanyiwest.jo@GMAIL.COM>
2017-10-18 00:04:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(()=>{
 	});
 });
 function getMovies(searchText) {
-	axios.get('http://www.omdbapi.com?s='+searchText)
+	axios.get('https://www.omdbapi.com?s='+searchText)
 		.then((response)=>{
 		console.log(response);
 		let movies=response.data.Search;
